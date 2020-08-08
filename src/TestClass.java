ccf15872e2fb1374145847b6fb1804657de66570
huseyin0865 <69403340+huseyin0865@users.noreply.github.com>
2020-08-09 00:02:10 +0200

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba1");
+         System.out.println("GitHub dan merhaba1");
     }
 }
